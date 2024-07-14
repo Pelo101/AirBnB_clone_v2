8b597acd8fc12b89abb840c16edbdb36958180d2
Pelo101 <110992136+Pelo101@users.noreply.github.com>
2024-07-14 08:31:11 -0700

    task 2

---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+"""Fabric script that distributes an archive to your web servers"""
+from fabric.api import run, put, env
+import os
+
+env.hosts = ['3.84.237.22', '52.86.145.163']
+
+
+def do_deploy(archive_path):
+    """Function that distributes and archive to web servers"""
+
+    if not os.path.exists(archive_path):
+        return False
+
+    archive_file = os.path.basename(archive_path)
+    archive_container = archive_file.split('.')[0]
+    release_folder = f'/data/web_static/releases/{archive_container}'
+
+    put(archive_path, '/tmp/')
+    run(f'mkdir -p {release_folder}')
+    run(f'tar -xzvf /tmp/{archive_file} -C {release_folder}')
+
+    run('rm -f /data/web_static/current')
+    run(f'ln -s {release_folder} /data/web_static/current')
+
+    return True
