575d9b9bc38e638d6a1b534e70ad5471aca05270
Pelo101 <110992136+Pelo101@users.noreply.github.com>
2024-08-04 04:06:00 -0700

    fix

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """ """
 from models.base_model import BaseModel
-from models.engine.file_storage import FileStorage
+from models import storage
 import unittest
 import datetime
 from uuid import UUID
