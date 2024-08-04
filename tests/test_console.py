f30fd54056c4a310d9cd3ed5ec9d42e57ddac470
Pelo101 <110992136+Pelo101@users.noreply.github.com>
2024-08-04 12:00:31 +0000

    update fix

--- tests/test_console.py
+++ tests/test_console.py
@@ -19,11 +19,10 @@ class TestHBNBCommand(unittest.TestCase):
         mock_instance = MagicMock()
         mock_instance.id = "1234"
 
-        """Configure the mock class to return the mock instance when instantiated"""
+        """Configure the mock class to return"""
 
         mock_class.return_value = mock_instance
         mock_classes['MyClass'] = mock_class
-
 
         command = HBNBCommand()
 
@@ -31,19 +30,19 @@ class TestHBNBCommand(unittest.TestCase):
 
         with patch('sys.stdout', new_callable=StringIO) as mock_stdout:
 
-             """Test argument"""
+            """Test argument"""
 
-             command.do_create('MyClass key="value" number=42')
+            command.do_create('MyClass key="value" number=42')
 
-             mock_class.assert_called_once()
-             mock_instance.configure_mock(**{'key': 'value', 'number': 42})
-             self.assertEqual(mock_instance.key, 'value')
-             self. assertEqual(mock_instance.number, 42)
-             mock_instance.save.assert_called_once()
+            mock_class.assert_called_once()
+            mock_instance.configure_mock(**{'key': 'value', 'number': 42})
+            self.assertEqual(mock_instance.key, 'value')
+            self. assertEqual(mock_instance.number, 42)
+            mock_instance.save.assert_called_once()
 
-             """check the printed output"""
-             output = mock_stdout.getvalue().strip()
-             self.assertEqual(output, "1234")
+            """check the printed output"""
+            output = mock_stdout.getvalue().strip()
+            self.assertEqual(output, "1234")
 
 
 if __name__ == '__main__':
