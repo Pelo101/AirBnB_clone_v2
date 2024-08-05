9c738567a125dbd7654864519d7f2e58b5e51bcf
Pelo101 <110992136+Pelo101@users.noreply.github.com>
2024-08-05 21:04:49 +0000

    fix indentation

--- tests/test_models/test_city.py
+++ tests/test_models/test_city.py
@@ -16,11 +16,11 @@ class test_City(TestBaseModel):
     def test_state_id(self):
         """ """
         new = self.value()
-	print(f"state_id type: {type(new.state_id}")
-        self.assertEqual(type(new.state_id), str)
+    print(f"state_id type: {type(new.state_id}")
+    self.assertEqual(type(new.state_id), str)
 
     def test_name(self):
         """ """
         new = self.value()
-	print(f"name type: {type(new.name)}")
-        self.assertEqual(type(new.name), str)
+    print(f"name type: {type(new.name)}")
+    self.assertEqual(type(new.name), str)
